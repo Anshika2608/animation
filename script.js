f36a2cf5bfee8a6a3338877414a711d8016da47e
Anshika2608 <anshika2210142@akgec.ac.in>
2023-10-01 22:02:42 +0530

    added overlap sign in and create account

---
+++ script.js
@@ -0,0 +1,29 @@
+// document.querySelector("#button1").onclick=function({
+//     const h1=document.querySelector("#sign");
+//     console.log(h1.style);
+// }
+// document.getElementById("sign").addEventListener("click",display);
+// function display(){
+//     document.getElementById("sign").style.z-index = '1';
+// }
+// {
+//     form-container sign-up.style.z-index="1";
+// })
+// let form-section = document.querySelector("#form-section");
+// let form-container sign-up= document.querySelector("#form-container sign-up");
+// form-section.addEventListener('click', function (){
+// form- container sign-up.style.z-index="1"
+// })
+// var form-section= document.querySelector(".form-section")
+// var sign-up= document.querySelector(".form-container sign-up")
+// form-section.addEventListener('click', function (){
+// form- container sign-up.style.zIndex="1"
+// })
+// var form= document.querySelector(".form-section")
+// var signup= document.querySelector("#sign")
+// form.addEventListener('click', function (){
+// sign.style.zIndex="-1"
+// })
+// function changez(){
+//     var element=document.
+// }
