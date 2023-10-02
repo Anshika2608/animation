ac2aa410e54e33e88de4ca94adce3c86b2a6ef68
Anshika2608 <anshika2210142@akgec.ac.in>
2023-10-02 11:50:40 +0530

    added sign-up button functionality

--- script.js
+++ script.js
@@ -15,15 +15,25 @@
 // form- container sign-up.style.z-index="1"
 // })
 // var form-section= document.querySelector(".form-section")
-// var sign-up= document.querySelector(".form-container sign-up")
+// var signup= document.querySelector(".form-container sign-up")
 // form-section.addEventListener('click', function (){
 // form- container sign-up.style.zIndex="1"
 // })
 // var form= document.querySelector(".form-section")
-// var signup= document.querySelector("#sign")
+// var signup= document.querySelector(".sign-up")
 // form.addEventListener('click', function (){
-// sign.style.zIndex="-1"
+// signup.style.zIndex="1";
 // })
-// function changez(){
-//     var element=document.
-// }
+
+function changezindex(){
+    document.getElementById("left").style.zIndex="-1";
+    document.getElementById("sign-in1").style.zIndex="-1";
+    document.getElementById("sign").style.zIndex="1";
+
+}
+function changeindex1(){
+    document.getElementById("right").style.zIndex="-1";
+    document.getElementById("sign").style.zIndex="-1";
+    document.getElementById("sign-in1").style.zIndex="1";
+
+}
