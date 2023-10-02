2e0d5e17c51a0621c618c8ba98bfb0e65ea0ffbe
Anshika2608 <anshika2210142@akgec.ac.in>
2023-10-02 16:09:31 +0530

    added transformation

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ function changezindex(){
     document.getElementById("sign").style.zIndex="1";
     document.getElementById("sign").style.transform="translateX(425px)";
     document.getElementById("right").style.transform="translateX(-425px)";
-    // document.getElementById("sign-in1").style.transform="translateX(-425px)";
-    // document.getElementById("left").style.transform="translateX(425px)";
+    //document.getElementById("sign-in1").style.transform="translateX(-425px)";
+    //document.getElementById("left").style.transform="translateX(425px)";
 }
 function changezindex1(){
     document.getElementById("left").style.zIndex="1";
@@ -17,5 +17,6 @@ function changezindex1(){
     document.getElementById("sign-in1").style.zIndex="1";
     document.getElementById("sign-in1").style.transform="translateX(-425px)";
     document.getElementById("left").style.transform="translateX(425px)";
-
+    // document.getElementById("sign").style.transform="translateX(425px)";
+    // document.getElementById("right").style.transform="translateX(-425px)";
 }
